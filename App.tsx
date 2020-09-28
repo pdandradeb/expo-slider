dbb76f5296a81d043cb5084412a685af73034344
Pedro Andrade <pdandradeb@gmail.com>
2020-09-28 15:25:46 -0300

    Adding slider

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
+import Slider from '@react-native-community/slider';
+
+const thumb = require('./assets/drag-button.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Image source={thumb} />
+      <Slider
+        style={{ width: '100%' }}
+        thumbImage={thumb}
+        minimumValue={0}
+        maximumValue={100}
+        step={1}
+        value={50}
+      />
       <StatusBar style="auto" />
     </View>
   );
